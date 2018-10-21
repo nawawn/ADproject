63821f0bedc2e6e895fbd7d332f3792369cb9865
Naw Awn <NAwn@britishmuseum.org>
2018-10-21 23:37:32 +0100

    Added Resolve-ADCanonicalName PowerShell script

---
+++ Resolve-ADCanonicalName.ps1
@@ -0,0 +1,14 @@
+﻿function Resolve-ADCanonicalName
+{
+    [CmdletBinding()]    
+    [OutputType([String])]
+    Param( 
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position=0)][Alias("CName")]$CanonicalName
+    )
+    return (Get-ADUser -Filter 'Enabled -eq $true' -Properties CanonicalName).Where{$_.CanonicalName -eq $CanonicalName} | Select -ExpandProperty Name    
+}
+
+#Resolve-CanonicalName -CName "adbm.britishmuseum.org/BM Users/Standard/ACottle"
