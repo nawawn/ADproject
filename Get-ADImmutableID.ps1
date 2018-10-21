a9f19c15619f35fd6e2027eb70bbae33a6976131
nawawn <nawawn@googlemail.com>
2018-10-22 00:28:01 +0100

    Added Get-ADImmutableID PowerShell Script

---
+++ Get-ADImmutableID.ps1
@@ -0,0 +1,19 @@
+﻿function Get-ADImmutableID{
+    [CmdletBinding()]    
+    [OutputType([String])]
+    Param(
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position=0)][Alias("Id")][String]$Identity
+    )
+    Try {
+        $ObjGuid = (Get-ADUser $Identity -Properties ObjectGUID).ObjectGUID
+        $SysGuid = New-Object -TypeName System.Guid -ArgumentList $ObjGuid
+        $ImmutableID = [Convert]::ToBase64String($SysGuid.ToByteArray())
+        return $ImmutableID
+    }
+    catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]{ 
+        Write-Output "The user doesn't exist!"
+    }
+}
